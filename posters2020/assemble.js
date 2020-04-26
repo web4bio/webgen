f107009ecb2de6300b77ec67ec537880171cb2ad
Jonas Almeida <jalmeida@mathbiol.org>
2020-04-26 00:05:39 -0400

    shorter rotation

--- posters2020/assemble.js
+++ posters2020/assemble.js
@@ -22,7 +22,7 @@ assemble=async(index='presentations.json')=>{
                 newDiv.setAttribute('data-x',Math.round(r*Math.cos(Math.PI*(i*360/n)/180)))
                 newDiv.setAttribute('data-y',Math.round(r*Math.sin(Math.PI*(i*360/n)/180)))
                 newDiv.setAttribute('data-z',2000-j*1000)
-                newDiv.setAttribute('data-rotate',90+i*360/n)
+                newDiv.setAttribute('data-rotate',-90+i*360/n)
                 //newDiv.setAttribute('data-x',Math.round(Math.random()*2000-1000))
                 //newDiv.setAttribute('data-y',Math.round(Math.random()*2000-1000))
                 //newDiv.setAttribute('data-z',Math.round(Math.random()*2000-1000))
@@ -32,7 +32,9 @@ assemble=async(index='presentations.json')=>{
             console.log(c,i,j,newDiv.id,newDiv)
             c++
         }
-
+        let overviewDiv = document.createElement('div')
+        overviewDiv.innerHTML=`<div id="overview_${i}" class="step" data-x="0000" data-y="0" data-z="460" data-rotate-x="0" data-rotate-y="-7" data-rotate-y="-10" data-rotate-z="-15" data-scale="7"></div>`
+        div.appendChild(overviewDiv.children[0])
         //debugger
     }
 
