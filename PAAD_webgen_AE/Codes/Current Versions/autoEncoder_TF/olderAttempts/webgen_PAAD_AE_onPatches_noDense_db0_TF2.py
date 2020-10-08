0b453894dbdb0aea04d229b3dbc62b0303efc294
skobayashi0417 <soma.kobayashi@stonybrookmedicine.edu>
2020-10-08 18:35:30 -0400

    ed

--- PAAD_webgen_AE/Codes/Current Versions/autoEncoder_TF/olderAttempts/webgen_PAAD_AE_onPatches_noDense_db0_TF2.py
+++ PAAD_webgen_AE/Codes/Current Versions/autoEncoder_TF/olderAttempts/webgen_PAAD_AE_onPatches_noDense_db0_TF2.py
@@ -18,22 +18,14 @@ import torch.nn.functional as F
 import glob
 import os
 
-def get_padding(X,expected_size=(520,280)):
-  print(X.shape[1])
-  print(X.shape[2])
-  widthCorrect = int((expected_size[1] - int(X.shape[2]))/2)
-  heightCorrect = int((expected_size[0]-int(X.shape[1]))/2)
-  return widthCorrect, heightCorrect
-
 class CVAE(tf.keras.Model):
   """Convolutional variational autoencoder."""
 
-  def __init__(self, latent_dim):
+  def __init__(self):
     super(CVAE, self).__init__()
-    self.latent_dim = latent_dim
     self.encoder = tf.keras.Sequential(
         [
-            tf.keras.layers.InputLayer(input_shape=(520, 280, 3)),
+            tf.keras.layers.InputLayer(input_shape=(400, 400, 3)),
             tf.keras.layers.Conv2D(
                 filters=32, kernel_size=3, padding='same', activation='relu'),
             tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
@@ -43,17 +35,12 @@ class CVAE(tf.keras.Model):
             tf.keras.layers.Conv2D(
                 filters=128, kernel_size=3, padding='same', activation='relu'),
             tf.keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
-            tf.keras.layers.Flatten(),
-            # No activation
-            tf.keras.layers.Dense(65*35),
         ]
     )
 
     self.decoder = tf.keras.Sequential(
         [
-            tf.keras.layers.InputLayer(input_shape=(65*35,)),
-            tf.keras.layers.Dense(units=65*35*128, activation=tf.nn.relu),
-            tf.keras.layers.Reshape(target_shape=(65, 35, 128)),
+            tf.keras.layers.InputLayer(input_shape=(50,50,128)),
             tf.keras.layers.Conv2DTranspose(
                 filters=64, kernel_size=2, strides=2,
                 activation='relu'),
@@ -68,12 +55,6 @@ class CVAE(tf.keras.Model):
 
   def call(self,X):
     print(X.shape)
-    widthCorrect, heightCorrect = get_padding(X=X)
-    print(widthCorrect)
-    print(heightCorrect)
-    paddings = tf.constant([[0,0],[heightCorrect,heightCorrect],[widthCorrect,widthCorrect],[0,0]])
-    print(paddings)
-    X = tf.pad(X,paddings,"CONSTANT")
     X = self.encoder(X)
     X = self.decoder(X)
     return X
@@ -98,6 +79,38 @@ class CVAE(tf.keras.Model):
       probs = tf.sigmoid(logits)
       return probs
     return logits
+  
+  optimizer = tf.keras.optimizers.Adam(1e-4)
+
+  def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2. * np.pi)
+    return tf.reduce_sum(
+        -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+        axis=raxis)
+
+
+  def compute_loss(model, x):
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+    x_logit = model.decode(z)
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+    logpz = log_normal_pdf(z, 0., 0.)
+    logqz_x = log_normal_pdf(z, mean, logvar)
+    return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+
+  @tf.function
+  def train_step(model, x, optimizer):
+    """Executes one training step and returns the loss.
+  
+    This function computes the loss and gradients, and uses the latter to
+    update the model's parameters.
+    """
+    with tf.GradientTape() as tape:
+      loss = compute_loss(model, x)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 optimizer = tf.keras.optimizers.Adam(1e-4)
 
@@ -142,16 +155,6 @@ def train_step(model, x, optimizer):
   gradients = tape.gradient(loss, model.trainable_variables)
   optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-def padding(img,expected_size = (280,520)):
-    desired_size_w = expected_size[0]
-    desired_size_h = expected_size[1]
-    #delta_width = desired_size_w - img.size[0]
-    #delta_height = desired_size_h - img.size[1]
-    #pad_width = delta_width //2
-    #pad_height = delta_height //2
-    #padding = (pad_width,pad_height,delta_width-pad_width,delta_height-pad_height)
-    #return ImageOps.expand(img, padding)
-    return ImageOps.pad(img,expected_size)
 
 def main():
     source = '/Users/soma/Desktop/WebgenThings/training_downsampled_tif_scalefactor_400/'
