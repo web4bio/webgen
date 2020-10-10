a5e14c376eab12878b0a592cbbf07968d07a525d
yuwei <yuwei.zhang@stonybrook.edu>
2020-10-10 17:03:02 -0400

    UPDATE

--- tensor/utils.py
+++ tensor/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from  matplotlib import pyplot as plt
 import os
 import tensorflow as tf
+import keras
 from keras.models import Model
 from keras.layers import Conv2D, MaxPool2D, Input, UpSampling2D
 from keras import layers, losses
