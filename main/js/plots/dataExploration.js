40cd7b068062892932d2d9cb3e1ac85a744b37cf
enemeth19 <erika.nemeth@stonybrook.edu>
2020-10-22 02:16:09 -0400

    Add grid for cohort exploration plots - A 3 column grid - For each new selection, a new plot is appended to grid

--- main/js/plots/dataExploration.js
+++ main/js/plots/dataExploration.js
@@ -5,49 +5,47 @@ let buildDataExplorePlots = async function() {
     }
 
     let mySelectedClinicalFeatures = $('.clinicalMultipleSelection').select2('data').map(clinicalInfo => clinicalInfo.text);
-    console.log(mySelectedClinicalFeatures)
 
-    console.log(clinicalQuery)
+    let currentFeature = mySelectedClinicalFeatures[mySelectedClinicalFeatures.length-1];
 
-    for(let j = 0; j < mySelectedClinicalFeatures.length; j++) {
-
-        let currentFeature = mySelectedClinicalFeatures[j];
-
-        let allX = []; 
-        for(let i = 0; i < clinicalQuery.length; i++) {
-            allX.push(clinicalQuery[i][currentFeature]);
-        }
-
-        console.log(allX)
-
-        let uniqueX = allX.filter(onlyUnique);
-    
-        let xCounts = [];
-        xCounts.length = uniqueX.length;
-        for(let i = 0; i < xCounts.length; i++)
-        xCounts[i] = 0;
-        
-        for(let i = 0; i < clinicalQuery.length; i++) 
-            for(let k = 0; k < uniqueX.length; k++) 
-                if(clinicalQuery[i][currentFeature] == uniqueX[k]) 
-                    xCounts[k]++;
-
-        var data = [{
-            values: xCounts,
-            labels: uniqueX,
-            type: 'pie',
-            textinfo: "label+percent",
-            textposition: "outside"
-        }];
-        
-        var layout = {
-            height: 400,
-            width: 500,
-            title: mySelectedClinicalFeatures[j] + "",
-            showlegend: false
-        };
-        
-        Plotly.newPlot('racePie', data, layout);
-
+    let allX = []; 
+    for(let i = 0; i < clinicalQuery.length; i++) {
+        allX.push(clinicalQuery[i][currentFeature]);
     }
+
+    let uniqueX = allX.filter(onlyUnique);
+
+    let xCounts = [];
+    xCounts.length = uniqueX.length;
+    for(let i = 0; i < xCounts.length; i++)
+    xCounts[i] = 0;
+    
+    for(let i = 0; i < clinicalQuery.length; i++) 
+        for(let k = 0; k < uniqueX.length; k++) 
+            if(clinicalQuery[i][currentFeature] == uniqueX[k]) 
+                xCounts[k]++;
+
+    var data = [{
+        values: xCounts,
+        labels: uniqueX,
+        type: 'pie',
+        textinfo: "label+percent",
+        textposition: "outside"
+    }];
+    
+    var layout = {
+        height: 400,
+        width: 500,
+        title: currentFeature + "",
+        showlegend: false
+    };
+
+    let parentRowDiv = document.getElementById("dataexploration");        
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "col s4");
+    newDiv.setAttribute("id", currentFeature + "Div");
+    parentRowDiv.appendChild(newDiv);
+    
+    Plotly.newPlot(currentFeature + 'Div', data, layout);
+
 }
