d510d512abe96be9846cc27d7fe1e70ee586454d
Carolyn Bremer <cgbrem@gmail.com>
2020-10-22 23:00:10 -0400

    buildHeatmap & buildViolinPlot implemented

--- main/js/docManipulation.js
+++ main/js/docManipulation.js
@@ -228,6 +228,47 @@ let buildPlots = async function() {
   });
 };
 
+buildHeatmap = async function(cohortQuery, data){
+  // Remove the loader
+  document.getElementById('heatmapDiv0').classList.remove('loader');
+
+  let svgHeatMap = d3.select("#heatmapRef").append("svg")
+        .attr("viewBox", `0 0 1250 500`)  // This line makes the svg responsive
+        .attr("id", 'svgHeatMap')
+
+  // Create the heatmap
+  createHeatmap('cohort', cohortQuery, data, svgHeatMap);
+};
+
+buildViolinPlot = async function(data, cohortQuery){
+  // Build the violin plot:
+  //Appending multiple g elements to svg object for violin plot
+  let myCohorts = d3.map(data, function(d){return d.cohort;}).keys();
+  //Define the number of cohorts to create a plot for
+  let numCohorts = myCohorts.length;
+  //Spacing between plots
+  let ySpacing = margin.top;
+
+  // Append an svg object for each cohort to create a violin plot for
+  for(var index = 0; index < numCohorts; index++)
+  {
+    //Define the current cohort to create the violin plot for
+    let curCohort = myCohorts[index];
+
+    let svgViolinPlot = d3.select("#violinPlotRef").append("svg")
+      .attr("viewBox", `0 0 1250 500`)  // This line makes the svg responsive
+      .attr("id", `svgViolinPlot${index}`)
+      .append("g")
+      .attr("transform",
+          "translate(" + (margin.left-20) + "," + 
+                      (margin.top + ySpacing*index*0.25) + ")");
+
+    // Create the violin plot:
+    createViolinPlot('cohort', cohortQuery, data, svgViolinPlot, curCohort);
+  }
+};
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////// Build Plots on Page (above) /////////////////////////////////////////////////////////////
