688effeffcd94369ea224d01d398870ecae8d8f6
enemeth19 <erika.nemeth@stonybrook.edu>
2020-10-30 03:12:16 -0400

    Add change color on click of pie sector

--- main/js/plots/dataExploration.js
+++ main/js/plots/dataExploration.js
@@ -30,14 +30,23 @@ let buildDataExplorePlots = async function() {
         labels: uniqueX,
         type: 'pie',
         textinfo: "label+percent",
-        textposition: "outside"
+        textposition: "outside",
+        marker: {
+            colors: ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+            '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'],
+            line: {
+                color: 'black', 
+                width: 1
+            }
+        }
     }];
     
     var layout = {
         height: 400,
         width: 500,
         title: currentFeature + "",
-        showlegend: false
+        showlegend: false,
+        extendpiecolors: true
     };
 
     let parentRowDiv = document.getElementById("dataexploration");        
@@ -50,10 +59,17 @@ let buildDataExplorePlots = async function() {
 
     document.getElementById(currentFeature + 'Div').on('plotly_click', function(data) {
         var pts = '';
+        var colore;
+        var tn = '';
         for(let i = 0; i < data.points.length; i++) {
-            pts = 'Label: ' + data.points[0].label + ' Value: ' + data.points[0].value;
+            pts = data.points[i].pointNumber;
+            tn = data.points[i].curveNumber;
+            colore = data.points[i].data.marker.colors;
         }
-        alert(pts);
+        colore[pts] = '#FFF34B';
+        var update = {'marker': {colors: colore, 
+                                line: {color: 'black', width: 1}}};
+        Plotly.restyle(currentFeature + 'Div', update, [tn]);
     });
 
 }
