c478099e429a78ecc5a50910c83bdad0448e00e7
yuwei <yuwei.zhang@stonybrook.edu>
2020-11-05 23:30:09 -0500

    refine moving

--- archive/yuwei/img-reaction/reaction.js
+++ archive/yuwei/img-reaction/reaction.js
@@ -51,22 +51,45 @@ function dragEvent(){
         y_1 = e1.clientY;
         canvas.onmousemove = function(e2){
             if(dragging){
+                // current image size
                 var imgW = img_w * scale_value;
                 var imgH = img_h * scale_value;
+                // current mouse position
                 x_2 = e2.clientX;
                 y_2 = e2.clientY;
+                // the displacement
                 var x = x_2 - x_1;
                 var y = y_2 - y_1;
-              
+                // location after the displacement
                 loc_x = dx + x;
                 loc_y = dy + y;
-
-                cor = "x1:"+x_1+"x2:"+x_2;
-                document.getElementById("pos").innerHTML=dx;
+                // make sure image won't be dragged out of the canvas
+                bound = canvas.width / 2 - imgH / 2;
+                if (loc_x < 2*bound ){
+                    loc_x = 2*bound;
+                };
+                if(loc_x > 0){
+                    loc_x = 0;
+                };
+                if(loc_y < 2*bound){
+                    loc_y = 2*bound;
+                };
+                if(loc_y > 0){
+                    loc_y = 0;
+                }
+                //debug
+                //cor = "x1:"+x_1+"x2:"+x_2;
+                //document.getElementById("pos").innerHTML=bound;
             }
             ctx.clearRect(0,0,canvas.width,canvas.height);
             ctx.drawImage(img, loc_x, loc_y, imgW, imgH);
+
         }
+        // renew position
+        dx = loc_x;
+        dy = loc_y;
+
+        // once stop dragging, stop all
         canvas.onmouseleave = function(){
             dragging=false;
             canvas.onmousemove=null;
