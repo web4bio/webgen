c19d1417e0867dc2a731aff2d3bca06c10616dba
kevin-murgas <kevin.murgas@stonybrookmedicine.edu>
2021-04-18 03:53:45 -0400

    added query functions to pull all clinical data (don't specify a clinical feature to get all)

--- main/js/dataAcquisition/fetchClinicalData.js
+++ main/js/dataAcquisition/fetchClinicalData.js
@@ -83,3 +83,75 @@ fetchClinicalData_cc = async function (cohortQuery, clinicalQuery) {
     }
 
 }
+
+// Function to fetch expression data from firebrowse, no clinical features specified:
+fetchClinicalData_b = async function (barcodeQuery) {
+
+    // Set up host and endpoint urls
+    const hosturl = 'https://firebrowse.herokuapp.com';
+    const endpointurl = 'http://firebrowse.org/api/v1/Samples/Clinical_FH'; //sample remainder of URL is: ?format=json&cohort=PRAD&fh_cde_name=psa_value&page=1&page_size=250&sort_by=cohort
+
+    // Set up endpoint url fields (except barcodes and clinical query) with preset values
+    const endpointurl_presets = {
+        format: 'json',
+        tcga_participant_barcode: barcodeQuery.join(","),
+        //fh_cde_name: clinicalQuery.join(","),
+        page: '1',
+        page_size: 2000,
+        sort_by: 'tcga_participant_barcode'
+    };
+
+    // Monitor the performance of the fetch:
+    const fetchStart = performance.now();
+
+    // Fetch data from stitched api:
+    var fetchedClinicalData = await fetch(hosturl + '?' + endpointurl + '?' + jQuery.param(endpointurl_presets));
+
+    // Monitor the performance of the fetch:
+    var fetchTime = performance.now() - fetchStart;
+    console.info("Performance of clinical data fetch: ");
+    console.info(fetchTime);
+
+    // Check if the fetch worked properly:
+    if (fetchedClinicalData == '')
+        return ['Error: Invalid Input Fields for Query.', 0];
+    else {
+        return fetchedClinicalData.json();
+    }
+}
+
+// Function to fetch expression data from firebrowse, no clinical features specified:
+fetchClinicalData_c = async function (cohortQuery) {
+
+    // Set up host and endpoint urls
+    const hosturl = 'https://firebrowse.herokuapp.com';
+    const endpointurl = 'http://firebrowse.org/api/v1/Samples/Clinical_FH'; //sample remainder of URL is: ?format=json&cohort=PRAD&fh_cde_name=psa_value&page=1&page_size=250&sort_by=cohort
+
+    // Set up endpoint url fields (except barcodes and clinical query) with preset values
+    const endpointurl_presets = {
+        format: 'json',
+        cohort: cohortQuery.join(","),
+        //fh_cde_name: clinicalQuery.join(","),
+        page: '1',
+        page_size: 2000,
+        sort_by: 'tcga_participant_barcode'
+    };
+
+    // Monitor the performance of the fetch:
+    const fetchStart = performance.now();
+
+    // Fetch data from stitched api:
+    var fetchedClinicalData = await fetch(hosturl + '?' + endpointurl + '?' + jQuery.param(endpointurl_presets));
+
+    // Monitor the performance of the fetch:
+    var fetchTime = performance.now() - fetchStart;
+    console.info("Performance of clinical data fetch: ");
+    console.info(fetchTime);
+
+    // Check if the fetch worked properly:
+    if (fetchedClinicalData == '')
+        return ['Error: Invalid Input Fields for Query.', 0];
+    else {
+        return fetchedClinicalData.json();
+    }
+}
--- main/js/dataAcquisition/getClinicalDataJSONarray.js
+++ main/js/dataAcquisition/getClinicalDataJSONarray.js
@@ -36,3 +36,20 @@ getClinicalDataJSONarray_cc = async function (cohortQuery, clinicalQuery) {
   console.log(results)
   return await results;
 };
+
+// functions to just query all of clinical data (don't specify any clinical feature) for cohort or barcodes
+getClinicalDataJSONarray_b = async function (barcodeQuery) {
+  var dataFetched = await fetchClinicalData_b(barcodeQuery);
+  // Remove the uppermost level of the data (cleaning)
+  var results = dataFetched.Clinical_FH;
+  console.log(results)
+  return await results;
+};
+
+getClinicalDataJSONarray_c = async function (cohortQuery) {
+  var dataFetched = await fetchClinicalData_c(cohortQuery);
+  // Remove the uppermost level of the data (cleaning)
+  var results = dataFetched.Clinical_FH;
+  console.log(results)
+  return await results;
+};
