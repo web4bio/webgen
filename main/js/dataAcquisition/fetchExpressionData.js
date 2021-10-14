48fa008c8d438c86bc896086bf73bc226359d2c4
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-10-14 18:14:11 -0400

    add jsdocs + use URLSearchParams + rm old comments
    This commit adds documentation in the style of jsdocs. Nice! To do this,
    I added `console.log` calls to print the variables that were going into
    and out of the function. That allowed me to figure out the types for
    arguments and the structure of the returned object. To describe the
    returned object, I used a typedef. This typedef defines one item in the
    returns object, and the returned object includes an array of these
    items.
    
    I have several questions after working on this function.
    1. How should we handle calls to `fetch`? In this function, we check
       whether the output is an empty string. But would it ever be? Would it
       make more sense to check the response's return code? Or perhaps use a
       try-catch and log errors?
    2. We return an array if `fetch` returns an empty string. Do we do
       anything with this array? It seems like a `console.log` would suffice
       here. Actually, I will look into the call stack and probably change
       this to a `console.log`.

--- main/js/dataAcquisition/fetchExpressionData.js
+++ main/js/dataAcquisition/fetchExpressionData.js
@@ -1,113 +1,49 @@
-// Executes a fetch for the following mRNASeq-associated data from Firebrowse as a 1-D array, where each element of the array 
-// is associated with a particular cohort-gene pair. Selections of cohort and gene are made by the user.
-    // cohort
-    // expression_log2
-    // gene
-    // geneID
-    // protocol
-    // sample_type
-    // tcga_participant_barcode
-    // z-score
-
-// Function to fetch expression data from firebrowse:
-fetchExpressionData_cg = async function(cohortQuery, geneQuery) {
-
+/** Fetch mRNA expression data for a set of cohorts and genes.
+ *
+ * @param {string|string[]} cohortQuery - Cohort(s) to fetch.
+ * @param {string|string[]} geneQuery - Gene(s) to fetch.
+ *
+ * @typedef {Object} mRNASeqItem
+ * @property {string} cohort
+ * @property {number} expression_log2
+ * @property {string} gene
+ * @property {number} geneID
+ * @property {string} protocol
+ * @property {string} sample_type
+ * @property {string} tcga_participant_barcode
+ * @property {number} z-score
+ *
+ * @returns {{mRNASeq: mRNASeqItem[]}|[]} Object with fetched data, or array with
+ *  error information.
+ *
+ * @example
+ *   fetchExpressionData_cg("BLCA", "bcl2")
+ *   fetchExpressionData_cg(["BLCA", "BRCA"], ["bcl2", "tp53"])
+**/
+async function fetchExpressionData_cg(cohortQuery, geneQuery) {
     // Set up host and endpoint urls
-    const hosturl = 'https://firebrowse.herokuapp.com';
-    const endpointurl='http://firebrowse.org/api/v1/Samples/mRNASeq';
-
-    // Set up endpoint url fields (except cohort and gene) with preset values
-    const endpointurl_presets = {
-        format: 'json',
+    const hosturl = "https://firebrowse.herokuapp.com";
+    const endpointurl = "http://firebrowse.org/api/v1/Samples/mRNASeq";
+    const params = new URLSearchParams({
+        format: "json",
         gene: geneQuery,
-        cohort: cohortQuery, 
-        sample_type: 'TP', 
-        protocol: 'RSEM',
-        page: '1',
+        cohort: cohortQuery,
+        sample_type: "TP",
+        protocol: "RSEM",
+        page: "1",
         page_size: 2001,
-        sort_by: 'tcga_participant_barcode' 
-    };
-
-    // Assemble a string by concatenating all fields and field values for endpoint url
-    const endpointurl_fieldsWithValues = 
-        'format=' + endpointurl_presets.format + 
-        '&gene=' + geneQuery + 
-        '&cohort=' + cohortQuery + 
-        '&sample_type=' + endpointurl_presets.sample_type +
-        '&protocol=' + endpointurl_presets.protocol +
-        '&page=' + endpointurl_presets.page + 
-        '&page_size=' + endpointurl_presets.page_size.toString() + 
-        '&sort_by=' + endpointurl_presets.sort_by;
-
-        
-    // Monitor the performance of the fetch:
-    const fetchStart = performance.now();
+        sort_by: "tcga_participant_barcode"
+    });
 
     // Fetch data from stitched api:
-    var fetchedExpressionData = await fetch(hosturl + '?' + endpointurl + '?' + endpointurl_fieldsWithValues);
-
-    // Monitor the performance of the fetch:
-    var fetchTime = performance.now() - fetchStart;
-    // console.info("Performance of fetch: ");
-    // console.info(fetchTime);
+    const fetchedExpressionData = await fetch(`${hosturl}?${endpointurl}?${params.toString()}`);
 
     // Check if the fetch worked properly:
-    if (fetchedExpressionData == '')
-        return ['Error: Invalid Input Fields for Query.', 0];
+    if (fetchedExpressionData === "") {
+        return ["Error: Invalid Input Fields for Query.", 0];
+    }
     else {
-        return fetchedExpressionData.json();
+        return await fetchedExpressionData.json();
     }
 
 }
-
-// fetchExpressionData_cgb = async function(cohortQuery, geneQuery, barcodes) {
-
-//     // Set up host and endpoint urls
-//     const hosturl = 'https://firebrowse.herokuapp.com';
-//     const endpointurl='http://firebrowse.org/api/v1/Samples/mRNASeq';
-
-//     // Set up endpoint url fields (except cohort and gene) with preset values
-//     const endpointurl_presets = {
-//         format: 'json',
-//         gene: geneQuery,
-//         cohort: cohortQuery,
-//         sample_type: 'TP',   
-//         tcga_participant_barcode: barcodes,   
-//         protocol: 'RSEM',
-//         page: '1',
-//         page_size: 2001,
-//         sort_by: 'tcga_participant_barcode' 
-//     };
-
-//     // Assemble a string by concatenating all fields and field values for endpoint url
-//     const endpointurl_fieldsWithValues = 
-//         'format=' + endpointurl_presets.format + 
-//         '&gene=' + geneQuery + 
-//         '&tcga_participant_barcode=' + barcodes +
-//         '&cohort=' + cohortQuery + 
-//         '&sample_type=' + endpointurl_presets.sample_type +
-//         '&protocol=' + endpointurl_presets.protocol +
-//         '&page=' + endpointurl_presets.page + 
-//         '&page_size=' + endpointurl_presets.page_size.toString() + 
-//         '&sort_by=' + endpointurl_presets.sort_by;
-
-
-//     // Monitor the performance of the fetch:
-//     const fetchStart = performance.now();
-
-//     // Fetch data from stitched api:
-//     var fetchedExpressionData = await fetch(hosturl + '?' + endpointurl + '?' + endpointurl_fieldsWithValues);
-
-//     // Monitor the performance of the fetch:
-//     var fetchTime = performance.now() - fetchStart;
-//     console.info("Performance of fetch: ");
-//     console.info(fetchTime);
-
-//     // Check if the fetch worked properly:
-//     if (fetchedExpressionData == '')
-//         return ['Error: Invalid Input Fields for Query.', 0];
-//     else {
-//         return fetchedExpressionData.json();
-//     }
-
-// }
