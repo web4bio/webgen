cc62fad71843515c48f3d7b79456dd700c923791
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-10-16 11:10:42 -0400

    add helper to fetch from firebrowse

---
+++ main/js/dataAcquisition/fetchHelper.js
@@ -0,0 +1,31 @@
+/** Perform a fetch from the FireBrowse API.
+ *
+ * @param {string} endpoint - API endpoint to query.
+ * @param {Object} params - Parameters of the query.
+ *
+ * @returns {Promise<Response>} The response.
+ *
+ * @example
+ *  await fetchFromFireBrowse("/Samples/mRNASeq", {
+ *      format: "json",
+ *      gene: geneQuery,
+ *      cohort: cohortQuery,
+ *      protocol: "RSEM",
+ *      page: "1",
+ *      page_size: 2001,
+ *      sort_by: "tcga_participant_barcode",
+ *    });
+ */
+async function fetchFromFireBrowse(endpoint, params) {
+  const base = "https://firebrowse.herokuapp.com";
+  // Remove a leading / in the endpoint so we don't have duplicate / in
+  // the url. Using // in a url is valid but it feels dirty.
+  if (endpoint.startsWith("/")) {
+    endpoint = endpoint.substring(1);
+  }
+  endpoint = `http://firebrowse.org/api/v1/${endpoint}`;
+  params = new URLSearchParams(params);
+  const url = `${base}?${endpoint}?${params.toString()}`;
+  console.log(`Fetch ${url}`);
+  return await fetch(url);
+}
