a6d4db466be710a44df1e5c07d0dcfb21a3fc7fd
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-10-17 13:45:52 -0400

    return json + handle errors

--- main/js/dataAcquisition/fetchHelper.js
+++ main/js/dataAcquisition/fetchHelper.js
@@ -3,7 +3,7 @@
  * @param {string} endpoint - API endpoint to query.
  * @param {Object} params - Parameters of the query.
  *
- * @returns {Promise<Response>} The response.
+ * @returns {Promise<Object>} The fetched data.
  *
  * @example
  *  await fetchFromFireBrowse("/Samples/mRNASeq", {
@@ -26,5 +26,22 @@ async function fetchFromFireBrowse(endpoint, params) {
   endpoint = `http://firebrowse.org/api/v1/${endpoint}`;
   params = new URLSearchParams(params);
   const url = `${base}?${endpoint}?${params.toString()}`;
-  return await fetch(url);
+
+  // We could use Array.at(-1) to get the last item, but that does not have broad
+  // browser support at this time.
+  const splits = endpoint.split("/");
+  const expectedKey = splits[splits.length - 1];
+  const minimal_json = { [expectedKey]: [] };
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    console.error(`Fetching ${expectedKey} data was unsuccessful.`);
+    return minimal_json;
+  }
+  const json = await response.json();
+  if (!json) {
+    console.log(`${expectedKey} is empty, returning an object with empty ${expectedKey} `);
+    return minimal_json;
+  }
+  return json;
 }
