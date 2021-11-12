ff0db83c1e2606b8ad6525335b23e248612300dd
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-11-12 11:11:29 -0500

    add state.query namespace for getting dom results

--- main/js/state.js
+++ main/js/state.js
@@ -47,52 +47,49 @@ const state = {
     },
   },
 
-  /**
-   * Get array of selected cohorts.
-   * @returns {string[]} Array of selected cohorts.
-   * @example
-   * state.cohortQuery
-   * // Expected result if one selected BRCA and PAAD: ["BRCA", "PAAD"]
-   */
-  get cohortQuery() {
-    return $(".cancerTypeMultipleSelection").select2("data").map(cohort => cohort.text.match(/\(([^)]+)\)/)[1]);
-  },
+  query: {
 
-  /**
-   * Get array of genes queried for mutations.
-   * @returns {string[]} Array of selected genes.
-   * @example
-   * state.mutationQuery
-   * // Expected result if one selected TP53: ["TP53"]
-   */
-  get mutationQuery() {
-    return $(".geneOneMultipleSelection").select2("data").map(gene => gene.text);
-  },
+    /**
+     * Get array of selected cohorts.
+     * @returns {string[]} Array of selected cohorts.
+     * @example
+     * state.cohortQuery
+     * // Expected result if one selected BRCA and PAAD: ["BRCA", "PAAD"]
+     */
+    get cohorts() {
+      return $(".cancerTypeMultipleSelection").select2("data").map(cohort => cohort.text.match(/\(([^)]+)\)/)[1]);
+    },
 
-  /**
-   * Get array of genes queried for expression.
-   * @returns {string[]} Array of selected genes.
-   */
-  get expressionQuery() {
-    return $(".geneTwoMultipleSelection").select2("data").map(gene => gene.text);
-  },
+    /**
+     * Get array of genes queried for mutations.
+     * @returns {string[]} Array of selected genes.
+     * @example
+     * state.mutationQuery
+     * // Expected result if one selected TP53: ["TP53"]
+     */
+    get mutations() {
+      return $(".geneOneMultipleSelection").select2("data").map(gene => gene.text);
+    },
 
-  /**
-   * Get array of pathways queried for expression.
-   * @returns {string[]} Array of selected pathways.
-   */
-  get pathwaysQuery() {
-    return $(".pathwayMultipleSelection").select2("data").map(pathway => pathway.id);
-  }
+    /**
+     * Get array of genes queried for expression.
+     * @returns {string[]} Array of selected genes.
+     */
+    get expressions() {
+      return $(".geneTwoMultipleSelection").select2("data").map(gene => gene.text);
+    },
+
+    /**
+     * Get array of pathways queried for expression.
+     * @returns {string[]} Array of selected pathways.
+     */
+    get pathways() {
+      return $(".pathwayMultipleSelection").select2("data").map(pathway => pathway.id);
+    },
+  },
 
 };
 
 Object.seal(state.tabs);
+Object.seal(state.query);
 Object.seal(state);
-
-/**
- * sampTrackVars
- * sortToggleDiv
- *
- * toggleClust
- */
