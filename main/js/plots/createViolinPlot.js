918f414e4e18a8960c9970cf8d598314278defdf
amaraim22 <51249187+amaraim22@users.noreply.github.com>
2021-12-08 23:26:06 -0500

    remove wrap()

--- main/js/plots/createViolinPlot.js
+++ main/js/plots/createViolinPlot.js
@@ -635,44 +635,6 @@ function findMatchByTCGABarcode(patient, clinicalData)
     return -1;
 }
 
-/** Helper function to create multi-line x-axis labels
- * 
- * unused function
- * 
- * @param {text} text - x-axis labels 
- * @param {number} width - length of the labels
- */
-function wrap(text, width)
-{
-    text.each(function()
-    {
-      var text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
-          line = [],
-          lineNumber = 0,
-          lineHeight = 1.1, // ems
-          y = text.attr("y"),
-          dy = parseFloat(text.attr("dy")),
-          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-      while (word = words.pop())
-      {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width)
-        {
-          line.pop();
-          tspan.text(line.join(" "));
-          line = [word];
-          tspan = text.append("tspan").attr("x", 0)
-                                        .attr("y", y)
-                                        .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                                        .text(word);
-        }
-      }
-    });
-  }
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////// End Of Program ///////////////////////////////////////////////////////////////
