c0c1699c4308654ab869388d366f0aa368a79b43
Erika Nemeth <erikanemeth99@gmail.com>
2025-07-11 23:49:34 -0400

    Add best fit lines

--- main/js/plots/createCoexpressionPlot.js
+++ main/js/plots/createCoexpressionPlot.js
@@ -8,7 +8,6 @@ const createCoexpressionPlot = async function (expressionData, clinicalAndMutati
 
     const divObject = d3.select("#coexpressionLoaderDiv").html("");
 
-
     ///////////////////////////////////
     // 1) SAMPLE TRACK SELECTOR SETUP
     ///////////////////////////////////
@@ -41,22 +40,6 @@ const createCoexpressionPlot = async function (expressionData, clinicalAndMutati
         .style('text-align', 'left');
 
     var div_selectBody = div_checklist.append('div').attr('class', 'clin_selector'); // This is where checkboxes go
-
-    // function renderCB(div_obj, id) {
-    //     const label = div_obj.append('div').attr("class", "checkbox-container");
-    //     const label2 = label.append('label');
-    //     label2.append('input')
-    //         .attr('type', 'checkbox')
-    //         .attr('class', 'myCheckbox')
-    //         .attr('value', id)
-    //         .on('change', function () {
-    //             updatePlot(); // Update plot when checkbox is changed
-    //         });
-    //     label2.append('span')
-    //         .text(' ' + id)
-    //         .style('font-weight', 'normal')
-    //         .style("color", "#5f5f5f");
-    // }
 
     function renderRadioButton(div_obj, id) {
         const label = div_obj.append('div').attr("class", "radio-container");
@@ -107,38 +90,96 @@ const createCoexpressionPlot = async function (expressionData, clinicalAndMutati
         }
     
         const rValue = pearsonCorrelation(xValues, yValues);
-    
         const selectedClinicalVar = document.querySelector('.myRadioButton:checked')?.value;
     
-        const clinicalData = clinicalAndMutationData.map(sample => ({
-            x: sample[selectedX],
-            y: sample[selectedY],
-            clinicalValue: selectedClinicalVar ? sample[selectedClinicalVar] : "None"
-        }));
+        // Join by TCGA participant barcode
+        const clinicalMap = new Map(clinicalAndMutationData.map(d => [d.tcga_participant_barcode, d]));
+
+        const combinedData = selectedX_expression.map((xObj, i) => {
+            const barcode = xObj.sample || xObj.tcga_participant_barcode;
+            const yObj = selectedY_expression.find(y => y.sample === barcode || y.tcga_participant_barcode === barcode);
+            const clinical = clinicalMap.get(barcode);
+
+            if (!yObj || !clinical) return null;
+
+            return {
+                x: xObj.expression_log2,
+                y: yObj.expression_log2,
+                clinicalValue: selectedClinicalVar ? clinical[selectedClinicalVar] ?? "NA" : "All"
+            };
+        }).filter(d => d !== null);
+
     
-        const uniqueClinicalValues = [...new Set(clinicalData.map(d => d.clinicalValue))];
-        const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(uniqueClinicalValues);
+        const uniqueGroups = [...new Set(combinedData.map(d => d.clinicalValue))];
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain(uniqueGroups);
     
-        const colors = clinicalData.map(d => colorScale(d.clinicalValue));
+        const traces = [];
     
-        const trace = {
-            x: xValues,
-            y: yValues,
+        // Scatter points
+        traces.push({
+            x: combinedData.map(d => d.x),
+            y: combinedData.map(d => d.y),
             mode: 'markers',
             type: 'scatter',
-            name: 'Gene Expression (log2)',
-            marker: { size: 12, color: colors },
-            text: clinicalData.map(d => `${selectedClinicalVar || "Clinical Feature"}: ${d.clinicalValue}`)
-        };
+            marker: {
+                size: 10,
+                color: combinedData.map(d => colorScale(d.clinicalValue))
+            },
+            text: combinedData.map(d => `${selectedClinicalVar || "Clinical Feature"}: ${d.clinicalValue}`),
+            name: '',
+            showlegend: false
+        });
     
-        const layout = { 
-            xaxis: { title: selectedX }, 
-            yaxis: { title: selectedY }, 
+        // Regression lines
+        if (selectedClinicalVar) {
+            for (const group of uniqueGroups) {
+                const groupData = combinedData.filter(d => d.clinicalValue === group);
+                if (groupData.length < 2) continue;
+    
+                const regression = d3.regressionLinear()
+                    .x(d => d.x)
+                    .y(d => d.y)(groupData);
+    
+                const lineX = regression.map(d => d[0]);
+                const lineY = regression.map(d => d[1]);
+    
+                traces.push({
+                    x: lineX,
+                    y: lineY,
+                    mode: 'lines',
+                    type: 'scatter',
+                    name: `Fit: ${group}`,
+                    line: { color: colorScale(group), width: 2, dash: 'solid' }
+                });
+            }
+        } else {
+            // Single global regression
+            const regression = d3.regressionLinear()
+                .x(d => d.x)
+                .y(d => d.y)(combinedData);
+    
+            const lineX = regression.map(d => d[0]);
+            const lineY = regression.map(d => d[1]);
+    
+            traces.push({
+                x: lineX,
+                y: lineY,
+                mode: 'lines',
+                type: 'scatter',
+                name: 'Fit (all data)',
+                line: { color: 'black', width: 2, dash: 'dot' }
+            });
+        }
+    
+        const layout = {
+            xaxis: { title: selectedX },
+            yaxis: { title: selectedY },
             title: { text: `Gene Expression (Log2) | r = ${rValue}` }
         };
     
-        Plotly.newPlot("coexpressionPanel", [trace], layout);
+        Plotly.newPlot("coexpressionPanel", traces, layout);
     }
+    
     
     ///////////////////////////////////
     // 3) DROPDOWNS FOR GENE SELECTION
